a0a2f2e75d1583431dd21a3085769ed051466f13
Nikolay Yarlychenko <yarlychenkonikolay@gmail.com>
2021-10-24 11:27:26 +0300

    Added simpleAddGetProductsTest

--- hw3/src/test/java/ru/akirakozov/sd/refactoring/servlet/ServletTest.java
+++ hw3/src/test/java/ru/akirakozov/sd/refactoring/servlet/ServletTest.java
@@ -1,9 +1,8 @@
 package ru.akirakozov.sd.refactoring.servlet;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -12,7 +11,23 @@ import java.sql.Statement;
 
 public class ServletTest {
 
-    @Before
+    private static final String TEST_PRODUCT_NAME = "Book";
+    private static final long TEST_PRODUCT_PRICE = 1000;
+
+    @BeforeEach
+    public void dropTable() {
+        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
+            String sql = "DROP TABLE IF EXISTS PRODUCT";
+            Statement stmt = c.createStatement();
+
+            stmt.executeUpdate(sql);
+            stmt.close();
+        } catch (Exception e) {
+            Assertions.fail();
+        }
+    }
+
+    @BeforeEach
     public void initialiseDatabaseIfNeeded() {
         try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
             String sql = "CREATE TABLE IF NOT EXISTS PRODUCT" +
@@ -24,20 +39,7 @@ public class ServletTest {
             stmt.executeUpdate(sql);
             stmt.close();
         } catch (Exception e) {
-            Assert.fail();
-        }
-    }
-
-    @AfterEach
-    public void dropTable() {
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-            String sql = "DROP TABLE PRODUCT";
-            Statement stmt = c.createStatement();
-
-            stmt.executeUpdate(sql);
-            stmt.close();
-        } catch (Exception e) {
-            Assert.fail();
+            Assertions.fail();
         }
     }
 
@@ -47,13 +49,54 @@ public class ServletTest {
             Statement stmt = c.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT");
 
-            Assert.assertEquals(0, rs.getFetchSize());
+            long total = 0;
+            while (rs.next()) {
+                total += 1;
+            }
+            Assertions.assertEquals(0, total);
 
             rs.close();
             stmt.close();
         } catch (Exception e) {
-            Assert.fail();
+            Assertions.fail();
         }
+    }
+
+    @Test
+    public void simpleAddGetProductsTest() {
+        try {
+            try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
+                String sql = "INSERT INTO PRODUCT " +
+                        "(NAME, PRICE) VALUES (\"" + TEST_PRODUCT_NAME + "\"," + TEST_PRODUCT_PRICE + ")";
+                Statement stmt = c.createStatement();
+                stmt.executeUpdate(sql);
+                stmt.close();
+            }
+        } catch (Exception e) {
+            Assertions.fail();
+        }
+
+
+        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
+            Statement stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT");
+
+            long total = 0;
+            while (rs.next()) {
+                total += 1;
+                String name = rs.getString("name");
+                int price = rs.getInt("price");
+                Assertions.assertEquals(TEST_PRODUCT_NAME, name);
+                Assertions.assertEquals(TEST_PRODUCT_PRICE, price);
+            }
+            Assertions.assertEquals(1, total);
+
+            rs.close();
+            stmt.close();
+        } catch (Exception e) {
+            Assertions.fail();
+        }
+
     }
 
 }
